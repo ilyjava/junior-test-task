b9a3287dc910ba4710b14cee0d9d94a711175b73
ilyjava <34170388+ilyjava@users.noreply.github.com>
2018-08-05 12:00:00 +0300

    passed tests

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
  */
 public class IntIterable implements Iterable<Integer> {
     int[] backed;
-
+    private int i = 0;
 
     public IntIterable(int[] backed) {
         this.backed = backed;
@@ -20,13 +20,16 @@ public class IntIterable implements Iterable<Integer> {
     private class IntIterator implements Iterator<Integer> {
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+        	return i < backed.length;
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+        	try {
+        		return backed[i++];
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}
+        	return null;
         }
 
         public void remove() {
